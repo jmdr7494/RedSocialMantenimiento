bfa9f0f8505492170c6235f3d624cc2eb46248db
alejandro <alejandro@alejandro-Aspire-5560>
2017-10-30 17:34:49 +0100

    muro administrador(salvo editar)

--- src/persistencia/DAOUsuario.java
+++ src/persistencia/DAOUsuario.java
@@ -1,16 +1,22 @@
 package persistencia;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.bson.BsonDocument;
+import org.bson.BsonString;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Component;
 
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.MongoClientURI;
 
 import auxiliares.Utilidades;
+import modelo.Publicacion;
 import modelo.Usuario;
 
 @Component
@@ -18,7 +24,7 @@ public class DAOUsuario {
 
     private List lista = new LinkedList();
     
-	public static void insert(Usuario usuario) {
+	//public static void insert(Usuario usuario) {
 	/**	BsonDocument bso=new BsonDocument();
 		bso.append("nombre", new BsonString(usuario.getNombre()));
 		String pwd1="1234";
@@ -30,7 +36,7 @@ public class DAOUsuario {
 		MongoCollection<BsonDocument>usuarios=broker.getCollection("Usuarios");
 		usuarios.insertOne(bso);
 		*/
-	}
+	//}
 	
 	public static Usuario insertUserConPWD(Usuario usuario, String pwd) {
 		
@@ -40,7 +46,7 @@ public class DAOUsuario {
 		bso.put("nombre", usuario.getNombre());
 		String pwdencriptada=Utilidades.Encriptar(pwd);
 		bso.append("pwd", pwdencriptada);
-		bso.append("email", usuario.getDireccion());
+		bso.append("email", usuario.getemail());
 		
 		MongoBroker broker= MongoBroker.get();
 		MongoCollection<Document>usuarios=broker.getCollection("Usuarios");
@@ -48,7 +54,7 @@ public class DAOUsuario {
 		
 		Usuario user=null;
 		try {
-			user = DAOUsuario.select(usuario.getDireccion(),pwd);
+			user = DAOUsuario.select(usuario.getemail(),pwd);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			//e.printStackTrace();
@@ -72,8 +78,8 @@ public class DAOUsuario {
 		bso2.append("pwd", new BsonString(pwdencriptada));
 		
 	
-		usuarios.insertOne(bso2);
-		*/
+		usuarios.insertOne(bso2);*/
+		
 	}
 
 	public static Usuario select(String email,String pwd) throws Exception {
@@ -132,15 +138,49 @@ public class DAOUsuario {
 		return result;
 	}
 	
-	public static void delete(Usuario usuario) {
-		Document bso=new Document();
+	public static void delete(String id) {
+		/*Document bso=new Document();
 		bso.append("nombre", usuario.getNombre());
 		
 		MongoBroker broker= MongoBroker.get();
 		MongoCollection<Document>usuarios=broker.getCollection("Usuarios");
-		usuarios.deleteOne(bso);
+		usuarios.deleteOne(bso);*/
+		MongoBroker broker= MongoBroker.get();
+		MongoCollection<Document>usuarios=broker.getCollection("Usuarios");
+		usuarios.deleteOne(new Document("_id", new ObjectId(id)));
 		
 	}
+public static ArrayList selectAll () {
+		
+		ArrayList<Usuario> result = new ArrayList<Usuario>();
+		MongoBroker broker = MongoBroker.get();
+		MongoCollection<Document> usuarios=broker.getCollection("Usuarios");
+		FindIterable<Document> it = usuarios.find();
+		MongoCursor<Document> cursor = it.iterator();
 	
+		while (cursor.hasNext()) {
+			Document doc = cursor.next();
+			ObjectId id = (ObjectId)doc.get( "_id" );
+			Usuario usu = new Usuario(id.toString(),doc.getString("nombre"), doc.getString("email"), doc.getString("pwd"));
+			result.add(usu);
+		}
+
+		return result;
+	}
+public static ObjectId insert(Usuario usuario) {
+	Document doc=new Document();
+	doc.append("nombre", usuario.getNombre());
+	doc.append("email", usuario.getemail());
+	doc.append("password", usuario.getPwd());
+	
+	
+	MongoBroker broker= MongoBroker.get();
+	MongoCollection<Document>usuarios=broker.getCollection("Usuarios");
+	usuarios.insertOne(doc);
+	
+	 ObjectId id = (ObjectId)doc.get( "_id" );
+	
+	 return id;
+}
 
 }
